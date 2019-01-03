796540a1f03feb372bcbd95135e1f04818943963
Kelly Shaw <kellyshaw@Kellys-MBP.localdomain>
2019-01-03 13:22:41 -0500

    Added Testing Project

---
+++ CussBuster.Tests/AdminServiceTests.cs
@@ -0,0 +1,15 @@
+using NUnit.Framework;
+
+namespace CussBuster.Tests
+{
+    [TestFixture]
+    public class AdminServiceTests
+    {
+        [Test]
+        public void TwoPlus2Equals4()
+        {
+            Assert.True(2 + 2 == 4);
+        }
+    }
+}
+
